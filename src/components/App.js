246f04c7a979adbb1c55677e70ccffc23e063018
Paweł <pawel.sikora1999@interia.pl>
2021-05-04 22:14:19 +0200

    Added rendering notes from array

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function CreateNotes(NoteItem){
+    return <Note key={NoteItem.key} title={NoteItem.title} content={NoteItem.content}/>
+}
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <Note />
+            {notes.map(CreateNotes)}
             <Footer />
         </div>
     );
